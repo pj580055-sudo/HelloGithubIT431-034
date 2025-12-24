69ab7dcbe5126deea13274d1de2c94739adb1370
duangkamol130262-sketch <duangkamol130262@gmail.com>
2025-12-24 13:08:54 +0700

    Updatehome.php from duangkamolbranch
    Corrected HTML structure and fixed typos in links.

--- home.php
+++ home.php
@@ -24,7 +24,7 @@
 				<li><a href="home.php">หน้าแรกddddd</li>
 				<li><a href="room.php">ห้องพัก&ราคา</li>
 				<li><a herf="guest.php">สมุดเยี่ยม</li>
-				<li><a href="contact.php">ติดต่ออออออออออออ</li>
+				<li><a href="contact.php">ติดต่ออออออออออออหาใครรึ</li>
 				</ul><p>
 				<?php include "login_form.php";?>
 			</td>
@@ -32,4 +32,5 @@
 	</table>
 
 <body>
+
 
