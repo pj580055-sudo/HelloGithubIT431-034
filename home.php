37f65afed4f13ab1107e78edc7b1ee51562f555d
duangkamol130262-sketch <duangkamol130262@gmail.com>
2025-12-24 13:36:09 +0700

    Fix HTML structure and update navigation links

--- home.php
+++ home.php
@@ -21,7 +21,7 @@
 					เชิญชมห้องตัวอย่าง ได้ทุกวัน <a href=amit.php>ติดต่อสอบถาม</a>	
 			</td>
 			<td width="190" bgcolor="#FFFF66"><ul>
-				<li><a href="home.php"> หน้าแรกที่กกก</li>
+				<li><a href="home.php"> หน้าแรกที่ใจจริง</li>
 				<li><a href="room.php">ห้องพัก&ราคา</li>
 				<li><a herf="guest.php">สมุดเยี่ยม</li>
 				<li><a href="contact.php">ติดต่ออออออออออออหาใครรึ</li>
@@ -32,6 +32,7 @@
 	</table>
 
 <body>
+
 
 
 
